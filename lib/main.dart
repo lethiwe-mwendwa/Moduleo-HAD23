1cd6b90ac7efe6215b67b235a207f50c35900e5f
Ethan van der Westhuizen <ethan.westhuzien@gmail.com>
2023-10-29 01:41:52 +0100

    Four col gridded layout

--- lib/main.dart
+++ lib/main.dart
@@ -47,19 +47,22 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 4, // Number of columns in the grid
         ),
+        itemCount: _counter, // Number of items in the grid
+        itemBuilder: (context, index) {
+          // Build individual grid items here
+          return Card(
+            child: Center(
+              child: Text(
+                'Item $index',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+            ),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
